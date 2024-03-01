d1946201c2bca8e3249761a815965cd735c81d44
mcgear <michael.gearhardt@gmail.com>
2024-03-01 12:43:36 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -2,6 +2,6 @@
 // export * from 'https://deno.land/std@0.216.0/encoding/base64.ts';
 export * as path from "https://deno.land/std@0.216.0/path/mod.ts";
 export * from "https://deno.land/x/fathym_common@v0.0.158/mod.ts";
-export * from "https://deno.land/x/fathym_everything_as_code@v0.0.350/mod.ts";
+export * from "https://deno.land/x/fathym_everything_as_code@v0.0.369/mod.ts";
 export * from "https://raw.githubusercontent.com/fathym-deno/deno_kv_oauth/main/mod.ts";
 export * from "https://raw.githubusercontent.com/fathym-deno/fresh/fathym-use/server.ts";
